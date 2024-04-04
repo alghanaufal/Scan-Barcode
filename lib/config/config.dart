afccd0ad30ad8f664391b939a985dba773fbefae
algha3 <anaufalhz03@gmail.com>
2024-04-04 11:28:06 +0700

    New Layout Test 1

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,4 +1,4 @@
-String token = '6115e1be32c2a05a0b647a5f';
+String token = 'your token';
 String project = 'simexscan';
-String appid = '660026712577ea5dfea74b03';
+String appid = 'appid';
 String fileUri = 'https://file.etter.cloud/d226fd9f5fcf8bc3cbdff22e2bd79efe/';
